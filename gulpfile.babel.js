0118cfb368936101861a0d2166225b12cffdd580
ramsua <richardramsua@gmail.com>
2020-06-24 06:08:16 -0300

    Se dividen el proyecto en dos secciones, Producción y Desarrollo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,31 +17,75 @@ import concat from "gulp-concat"
 // Imágenes
 import imagemin from "gulp-imagemin"
 
+// Cache bust
+import cacheBust from 'gulp-cache-bust'
+
+// Browser Sync
+import { } from 'browser-sync'
+
 // Constantes
-const cssPlugins = [
+const cssPluginsProduction = [
     cssnano(),
     autoprefixer()
 ]
+const cssPluginsDevelopment = [
+    autoprefixer()
+]
 
-gulp.task("html", () => {
+// HTML
+// Production
+gulp.task("html-production", () => {
     return gulp
         .src("./src/*.html")
         .pipe(htmlmin({
             collapseWhitespace: true, // false o borrar para que no minifique
             removeComments: true // false o borrar para dejar comentarios
         }))
+        .pipe(cacheBust({
+            type: 'timestamp'
+        }))
+        .pipe(gulp.dest("./public"))
+})
+// Development
+gulp.task("html-dev", () => {
+    return gulp
+        .src("./src/*.html")
+        .pipe(cacheBust({
+            type: 'timestamp'
+        }))
         .pipe(gulp.dest("./public"))
 })
 
-gulp.task("css", () => {
+// CSS
+// Production
+gulp.task("css-production", () => {
     return gulp
         .src("./src/css/styles.css")
         .pipe(concat("styles.min.css"))
-        .pipe(postcss(cssPlugins))
+        .pipe(postcss(cssPluginsProduction))
+        .pipe(gulp.dest("./public/css"))
+})
+// Development
+gulp.task("css-dev", () => {
+    return gulp
+        .src("./src/css/styles.css")
+        .pipe(concat("styles.min.css"))
+        .pipe(postcss(cssPluginsDevelopment))
         .pipe(gulp.dest("./public/css"))
 })
 
-gulp.task("scripts", () => {
+// JavaScript
+// Production
+gulp.task("scripts-production", () => {
+    return gulp
+        .src("./src/js/*.js")
+        .pipe(concat("scripts.min.js"))
+        .pipe(babel())
+        .pipe(terser())
+        .pipe(gulp.dest("./public/js"))
+})
+// Development
+gulp.task("scripts-dev", () => {
     return gulp
         .src("./src/js/*.js")
         .pipe(concat("scripts.min.js"))
@@ -50,7 +94,9 @@ gulp.task("scripts", () => {
         .pipe(gulp.dest("./public/js"))
 })
 
-gulp.task("images", () => {
+// Images
+// Production
+gulp.task("images-production", () => {
     return gulp
         .src("src/assets/images/**/*")
         .pipe(imagemin([
@@ -66,3 +112,14 @@ gulp.task("images", () => {
         ]))
         .pipe(gulp.dest("public/assets/images"))
 })
+// Development
+gulp.task("images-dev", () => {
+    return gulp
+        .src("src/assets/images/**/*")
+        .pipe(gulp.dest("public/assets/images"))
+})
+
+// Watchers
+// gulp.task('production', () => {
+//     gulp.watch('')
+// })
